eafe02f78ffa669ba99aa22a14b18c697aefbb24
Aleksander Adamowski <olo@fb.com>
2021-01-12 19:53:34 -0800

    Add a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+import BlogLayout from "gatsby-theme-blog/src/components/layout"
+
+const NotFoundPage = (props) => (
+    <BlogLayout location={props.location} title="Aleksander Adamowski - not found">
+    <main>
+        <h1>Page not found</h1>
+        <p>
+            Please check that the URL you got is correct.
+        </p>
+    </main>
+    </BlogLayout>
+)
+export default NotFoundPage
